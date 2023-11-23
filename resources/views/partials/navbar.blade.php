db2274bac698529a097751e99b76633495f59723
paulina <paulinalestarisimatupang@gmail.com>
2023-11-23 09:08:49 +0700

    p

--- resources/views/partials/navbar.blade.php
+++
@@ -1,5 +0,0 @@
-<ul class="nav nav-tabs">
-    <li class="nav-item">
-        <a class="nav-link <?php echo (strpos($_SERVER['REQUEST_URI'], 'barang') !== false) ? 'active' : ''; ?>" href="/barang">Data Barang</a>
-    </li>
-</ul>
